88544368eb1954f3e9a90461d098d1ab7eba2bec
Ellpeck <megamaximal@gmail.com>
2020-05-09 01:21:16 +0200

    Revert "network lock equality, for the future"
    This reverts commit 58558781

--- src/main/java/de/ellpeck/prettypipes/network/NetworkLock.java
+++ src/main/java/de/ellpeck/prettypipes/network/NetworkLock.java
@@ -1,7 +1,5 @@
 package de.ellpeck.prettypipes.network;
 
-import java.util.Objects;
-
 public class NetworkLock {
 
     public final NetworkLocation location;
@@ -12,19 +10,5 @@ public class NetworkLock {
         this.location = location;
         this.slot = slot;
         this.amount = amount;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o instanceof NetworkLock) {
-            NetworkLock that = (NetworkLock) o;
-            return this.slot == that.slot && this.amount == that.amount && this.location.equals(that.location);
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.location, this.slot, this.amount);
     }
 }
